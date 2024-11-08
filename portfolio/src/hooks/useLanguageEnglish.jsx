06579221a8351e4c8be1b5b1e6449c6afd91eea9
CihangirKELES <cihangirkeles@msn.com>
2024-11-08 14:04:59 +0300

    delete

--- portfolio/src/hooks/useLanguageEnglish.jsx
+++
@@ -1,9 +0,0 @@
-import useLocalStorage from './useLocalStorage'
-
-const useLanguageEnglish = (initialState) => {
-  const [english, setEnglish] = useLocalStorage('english', initialState)
-
-  return [english, setEnglish]
-}
-
-export default useLanguageEnglish
